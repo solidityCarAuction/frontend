eee387a3b798f2cd5c623db8ae77030fdcfedbe5
morirokim <morirokim@gmail.com>
2025-04-24 10:28:18 +0900

    delete: remove useless methods

--- cart_auction/src/auctionInstance.ts
+++ cart_auction/src/auctionInstance.ts
@@ -2,7 +2,7 @@ import Web3 from "web3";
 import AuctionABI from "./dataset/abi.json";
 import { AbiItem } from "web3-utils";
 
-const CONTRACT_ADDRESS = "0x8B14B5073f30202AEe3Dba0f42abA525AcD92670"; // 배포된 컨트랙트의 주소값
+const CONTRACT_ADDRESS = "0x5e68Ef87bCB0a3c9e52B2f4Cfe17B8846Fc535ac"; // 배포된 컨트랙트의 주소값
 
 export const web3 = new Web3(new Web3.providers.WebsocketProvider("ws://localhost:7545"));
 
